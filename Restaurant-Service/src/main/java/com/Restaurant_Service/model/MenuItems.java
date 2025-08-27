3f52f2ca32553c03b32e6b1bc5a31ca381cfa076
faizan alam <alamf6023@gmail.com>
2025-08-27 11:26:28 +0530

    model work done

--- Restaurant-Service/src/main/java/com/Restaurant_Service/model/MenuItems.java
+++ Restaurant-Service/src/main/java/com/Restaurant_Service/model/MenuItems.java
@@ -1,12 +1,9 @@
 package com.Restaurant_Service.model;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,10 +11,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
@@ -25,30 +23,25 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name="restaurants", schema="practise_food_restaurants")
-public class Restaurant {
+@Table(name="menu_items",schema="practise_food_restaurants")
+public class MenuItems {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    @OneToMany(mappedBy="restaurant")
-    @JsonIgnore
-    private List<MenuItems> items;
+    @ManyToOne
+    @JoinColumn(name="restaurant_id",nullable=false)
+    private Restaurant restaurant;
 
-    @Column(nullable=false,unique=true)
-    private String restaurant_code;
+    @Column(nullable=false,unique=false)
+    private String name;
 
-    @Column(nullable=false)
-    private String restaurant_name;
-
-    @Column(nullable=true)
-    private String address;
+    @Column(nullable=false,unique=false)
+    private double price;
     
-    @Column(nullable=true)
-    private String phNumber;
-
-    private int rating;
+    @Column(nullable=false,unique=false)
+    private Boolean availablity;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
