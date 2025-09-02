fec4d966f795b41ef374864143b1102224ae8a7d
faizan alam <alamf6023@gmail.com>
2025-09-02 12:20:40 +0530

    repository implemented

--- Order-Service/src/main/java/com/Order_Service/repository/OrderRepo.java
+++ Order-Service/src/main/java/com/Order_Service/repository/OrderRepo.java
@@ -1,9 +1,20 @@
 package com.Order_Service.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.Order_Service.config.OrderStatus;
 import com.Order_Service.model.Orders;
 
+@Repository
 public interface OrderRepo extends JpaRepository<Orders, Integer> {
+
+    // List<Orders> findByStatus(OrderStatus status);
+    @Query("SELECT o FROM Orders o WHERE o.status = :status")
+    List<Orders> findByStatus(@Param("status") OrderStatus status);
     
 }
