937c48bf8232e9649f53edeb6d46c2ac36e17f7b
faizan alam <alamf6023@gmail.com>
2025-09-06 11:11:06 +0530

    done with crud of order items

--- Order-Service/src/main/java/com/Order_Service/controller/OrderItemController.java
+++ Order-Service/src/main/java/com/Order_Service/controller/OrderItemController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.Order_Service.service.OrderItemService;
 
@@ -22,10 +27,32 @@ public class OrderItemController {
     @Autowired
     private OrderItemService service;
     
-    @GetMapping("/allitems")
+    @GetMapping("/items")
     public ResponseEntity<List<OrderItemDTO>> getAllOrderItems() {
         List<OrderItemDTO> order = service.viewAllItems();
         return new ResponseEntity<>(order,HttpStatus.OK);
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<OrderItemDTO> addOrderItemsDetails(OrderItemDTO dto) {
+        return new ResponseEntity<>(service.addOrdersItems(dto),HttpStatus.OK);
+    }
+
+    @PutMapping("/item/{id}")
+    public ResponseEntity<?> updateItemDetail(@RequestBody OrderItemDTO dto, @PathVariable int id) {
+        OrderItemDTO order = service.updateItem(dto,id);
+        if(order != null) {
+            return new ResponseEntity<>(order,HttpStatus.OK);
+        }
+        else{
+            String msg = "Unable to Find the Data in our record!";
+            return new ResponseEntity<>(msg,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/item/{id}")
+    public ResponseEntity<String> deleteItemDetails(@PathVariable int id) {
+        return new ResponseEntity<>(service.deleteItem(id),HttpStatus.OK);
     }
     
 }
--- Order-Service/src/main/java/com/Order_Service/service/OrderItemService.java
+++ Order-Service/src/main/java/com/Order_Service/service/OrderItemService.java
@@ -11,7 +11,7 @@ import com.Order_Service.repository.OrderItemRepo;
 
 @Service
 public class OrderItemService {
-    
+
     @Autowired
     private OrderItemRepo repo;
 
@@ -23,7 +23,39 @@ public class OrderItemService {
         return dto;
     }
 
+    private OrderItems changeToEntity(OrderItemDTO dto) {
+        OrderItems entity = new OrderItems();
+        entity.setOrder_id(dto.getOrder_id());
+        entity.setQuantity(dto.getQuantity());
+        entity.setPrice(dto.getPrice());
+        return entity;
+    }
+
     public List<OrderItemDTO> viewAllItems() {
         return repo.findAll().stream().map(this::changeToDTO).toList();
+    }
+
+    public OrderItemDTO addOrdersItems(OrderItemDTO dto) {
+        OrderItems order = repo.save(changeToEntity(dto));
+        return changeToDTO(order);
+    }
+
+    public OrderItemDTO updateItem(OrderItemDTO dto, int id) {
+        if (repo.existsById(id)) {
+            OrderItems order = repo.findById(id).get();
+            order.setQuantity(dto.getQuantity());
+            order.setPrice(dto.getPrice());
+            OrderItems data = repo.save(order);
+            return changeToDTO(data);
+        }
+        return null;
+    }
+
+    public String deleteItem(int id) {    
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return "Item is Deleted Successfully!";
+        }
+        return "Unable to Find the Data in our record!";
     }
 }
